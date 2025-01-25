ada367d8fa42c02f1acb3e8f96385c0826edf8b7
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-01-24 19:46:44 -0500

    chore: agregar apps

---
+++ ciudadano_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CiudadanoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ciudadano_app'
---
+++ entidad_municipal_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EntidadMunicipalAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'entidad_municipal_app'
