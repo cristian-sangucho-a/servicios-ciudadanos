fc59f173a1b8b328fb5a7a198e7216eb223c5cad
cjzam <cristian.zambrano@epn.edu.ec>
2025-01-25 17:54:41 -0500

    test: implementar los steps para los escenarios reservar area comunar para actividad privada y publica del feature en la versión 1.0

--- features/steps/realizar_actividades_grupales.py
+++ features/steps/realizar_actividades_grupales.py
@@ -1,49 +1,54 @@
 from behave import *
 
-use_step_matcher("re")
+#use_step_matcher("re")
 
 
-@step('que existen areas comunales en el espacio publico "Parque la Alameda" disponibles en la ciudad y son')
+@step('que existen areas comunales en el espacio publico "{nombre_espacio_publico}" disponibles en la ciudad y son')
+def step_impl(context, nombre_espacio_publico):
+    context.ciudad = Ciudad("Quito")
+    context.espacio_publico = EspacioPublico(nombre_espacio_publico)
+    context.cancha1 = AreaComunal(context.table[0].cells[0])
+    context.cancha2 = AreaComunal(context.table[1].cells[0])
+    context.cancha3 = AreaComunal(context.table[2].cells[0])
+
+    context.espacio_publico.agregar_area_comunal(context.cancha1)
+    context.espacio_publico.agregar_area_comunal(context.cancha2)
+    context.espacio_publico.agregar_area_comunal(context.cancha3)
+
+    context.cancha1.agregar_fecha_disponible("2021-06-01", "12:00", "18:00")
+    context.cancha2.agregar_fecha_disponible("2021-06-01", "12:00", "18:00")
+    context.cancha3.agregar_fecha_disponible("2021-06-01", "12:00", "18:00")
+
+    assert context.ciudad.espacio_publico.hay_areas_comunales_disponibles();
+
+
+@step('el ciudadano no supera las "{maximo_reservas}" reservas activas')
+def step_impl(context, maximo_reservas):
+    context.ciudadano = Ciudadano("Fernando Do Santos", "1725548763")
+    assert context.ciudadano.obtener_numero_reservas_activas() <= maximo_reservas
+
+
+@step('el ciudadano realice una reserva "{tipo_reserva}" en el area comunal "{area_comunal}" el "{fecha_reserva}" de "{hora_inicio}" a "{hora_fin}"')
+def step_impl(context, tipo_reserva, area_comunal, fecha_reserva, hora_inicio, hora_fin):
+    pass #da valor en el siguiente paso
+
+
+@step("se guarda la reserva en la Agenda Pública.")
 def step_impl(context):
-    pass
+    context.controlador_reserva = ControladorReserva()
+    assert context.controlador_reserva.reservar_area_comunal(context.cancha1, context.fecha_reserva, context.hora_inicio, context.hora_fin, context.tipo_reserva, context.ciudadano)
 
 
-@step('cada ciudadano puede tener hasta "3" reservas activas')
+@step('agregue los correos de los invitados "{correos_invitados}" a la reserva')
+def step_impl(context, correos_invitados):
+    context.correos_invitados = correos_invitados
+    pass #esto puede ser verificado en el siguiente paso
+
+@step("se enviará una invitación por correo con los detalles de la reserva.")
 def step_impl(context):
-    pass
+    context.controlador_notificacion = ControladorNotificacion()
+    assert context.controlador_notificacion.enviar_invitacion(context.ciudadano.obtener_ultima_reserva())
 
-
-@step('el ciudadano tiene "2" reservas activas')
-def step_impl(context):
-    pass
-
-
-@step('el ciudadano reserve el area comunal "Cancha #1" el "15/01/2024" de "16:00" a "17:00"')
-def step_impl(context):
-    pass
-
-
-@step("se guarda la reserva en la Agenda Pública")
-def step_impl(context):
-    pass
-
-
-@step("se publicará la reserva en la Agenda Pública\.")
-def step_impl(context):
-    pass
-
-
-@step('agregue los correos de los invitados "jean\.cotera@epn\.edu, cristian\.sangucho@epn\.edu\.ec" a la reserva')
-def step_impl(context):
-    pass
-
-@step("se enviará una invitación por correo con los detalles de la reserva\.")
-def step_impl(context):
-    pass
-
-@step("se guarda la reserva en la Agenda Pública\.")
-def step_impl(context):
-    pass
 
 @step('que el ciudadano tiene una reserva en la "Cancha #1" en el espacio publico "Parque la Alameda"')
 def step_impl(context):
@@ -53,15 +58,15 @@ def step_impl(context):
 def step_impl(context):
     pass
 
-@step("la reserva será eliminada de la agenda pública\.")
+@step("la reserva será eliminada de la agenda pública.")
 def step_impl(context):
     pass
 
-@step("reserva será eliminada de la agenda pública\.")
+@step("reserva será eliminada de la agenda pública.")
 def step_impl(context):
     pass
 
-@step("se enviará una correo de cancelacion de cancelación a los invitados\.")
+@step("se enviará una correo de cancelacion de cancelación a los invitados.")
 def step_impl(context):
     pass
 
