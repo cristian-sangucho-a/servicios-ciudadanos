c304384245c2d793ebbf10269432c39f84417a6b
juanfcarrillo <juanfi444@outlook.com>
2025-01-25 01:28:47 -0500

    fix(setup.bat): changed to exec

--- setup.bat
+++ setup.bat
@@ -1,11 +1,7 @@
-@echo off
-REM Install Python version using pyenv-win
 pyenv install 3.12.5
 pyenv local 3.12.5
 
-REM Create and activate virtual environment
-python -m venv .venv
+pyenv exec python -m venv .venv
 call .venv\Scripts\activate
 
-REM Install dependencies
 pip install -r requirements.txt
