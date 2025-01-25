322434378cca6e5b14a5769ffc5388cd864a85e1
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-01-25 13:02:00 -0500

    test: crear los pasos de todos los escenarios

---
+++ features/steps/realizar_actividades_grupales.py
@@ -0,0 +1,70 @@
+from behave import *
+
+use_step_matcher("re")
+
+
+@step('que existen areas comunales en el espacio publico "Parque la Alameda" disponibles en la ciudad y son')
+def step_impl(context):
+    pass
+
+
+@step('cada ciudadano puede tener hasta "3" reservas activas')
+def step_impl(context):
+    pass
+
+
+@step('el ciudadano tiene "2" reservas activas')
+def step_impl(context):
+    pass
+
+
+@step('el ciudadano reserve el area comunal "Cancha #1" el "15/01/2024" de "16:00" a "17:00"')
+def step_impl(context):
+    pass
+
+
+@step("se guarda la reserva en la Agenda Pública")
+def step_impl(context):
+    pass
+
+
+@step("se publicará la reserva en la Agenda Pública\.")
+def step_impl(context):
+    pass
+
+
+@step('agregue los correos de los invitados "jean\.cotera@epn\.edu, cristian\.sangucho@epn\.edu\.ec" a la reserva')
+def step_impl(context):
+    pass
+
+@step("se enviará una invitación por correo con los detalles de la reserva\.")
+def step_impl(context):
+    pass
+
+@step("se guarda la reserva en la Agenda Pública\.")
+def step_impl(context):
+    pass
+
+@step('que el ciudadano tiene una reserva en la "Cancha #1" en el espacio publico "Parque la Alameda"')
+def step_impl(context):
+    pass
+
+@step("cancele la reserva")
+def step_impl(context):
+    pass
+
+@step("la reserva será eliminada de la agenda pública\.")
+def step_impl(context):
+    pass
+
+@step("reserva será eliminada de la agenda pública\.")
+def step_impl(context):
+    pass
+
+@step("se enviará una correo de cancelacion de cancelación a los invitados\.")
+def step_impl(context):
+    pass
+
+@step('cada ciudadano puede tener hasta "3" reservas activas')
+def step_impl(context):
+    pass
