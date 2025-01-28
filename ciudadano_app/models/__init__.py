0088ecb2d279d529f70d5f611347ae59a4c7b65a
Jorman Chuquer <jorman.chuquer@epn.edu.ec>
2025-01-28 17:40:41 -0500

    fix (models): importar en un orden específico para no romper las relaciones con la bd 🤮

--- ciudadano_app/models/__init__.py
+++ ciudadano_app/models/__init__.py
@@ -3,14 +3,12 @@ Inicializa el paquete de modelos para la aplicación ciudadano_app.
 """
 
 # ciudadano_app/models/__init__.py
-
-# Importaciones relativas desde submodulos
-from .ciudadano.ciudadano import Ciudadano
-from .ciudadano.gestor_ciudadano import GestorCiudadano
-from .reserva import Reserva
-from .area_comunal import AreaComunal
-from .reporte.tipo_reporte import TipoReporte
-from .reporte.reporte import Reporte
+from ciudadano_app.models.ciudadano.ciudadano import Ciudadano
+from ciudadano_app.models.ciudadano.gestor_ciudadano import GestorCiudadano
+from ciudadano_app.models.area_comunal import AreaComunal
+from ciudadano_app.models.reserva import Reserva
+from ciudadano_app.models.reporte.tipo_reporte import TipoReporte
+from ciudadano_app.models.reporte.reporte import Reporte
 
 __all__ = [
     "Ciudadano",
