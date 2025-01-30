e3e15d5b85664b04bf1bbf215f5788d4d6012901
Marco Márquez <m.marquez.desarrollador@outlook.com>
2025-01-29 21:53:04 -0500

    feat(canales): suscripción a canales y gestión de noticias (#10)
    Co-authored-by: moises7651 <moises.arequipa@epn.edu.ec>
    Co-authored-by: ESCRITORIO VIRTUAL 8 <escritorio.virtual8@epn.edu.ec>
    Co-authored-by: Juan F. Carrillo <78522923+juanfcarrillo@users.noreply.github.com>

--- entidad_municipal_app/models/__init__.py
+++ entidad_municipal_app/models/__init__.py
@@ -5,5 +5,9 @@ Modelos de la aplicación de entidad municipal.
 from .EntidadMunicipal import EntidadMunicipal
 from .evento_municipal import EventoMunicipal
 from .registro_asistencia import RegistroAsistencia
+from .canales.canal_informativo import CanalInformativo, Suscripcion
+from .canales.noticia import Noticia
+from .canales.reaccion import Reaccion
+from .canales.comentario import Comentario
 
-__all__ = ['EntidadMunicipal', 'EventoMunicipal', 'RegistroAsistencia']
+__all__ = ['EntidadMunicipal', 'EventoMunicipal', 'RegistroAsistencia','CanalInformativo','Suscripcion','Noticia','Reaccion','Comentario']
