566d1cd362ccee993559a9d8a3a1a59048036ed3
Jorman Chuquer <jorman.chuquer@epn.edu.ec>
2025-01-30 18:22:36 -0500

    feat (model): agregar principales overrides que se realizan con el ORM de django

--- ciudadano_app/models/reserva/servicio_reserva.py
+++ ciudadano_app/models/reserva/servicio_reserva.py
@@ -6,7 +6,7 @@ class ServicioReserva(RespositorioReserva):
     MAXIMO_RESERVAS = 3
 
     def ciudadano_supera_maximo_reservas(self, ciudadano: Ciudadano):
-        return ciudadano.obtener_reservas_activas() > self.MAXIMO_RESERVAS
+        return ciudadano.reservas.filter(estado_reserva='Activa').count() > self.MAXIMO_RESERVAS
 
     def reservar_area_comunal(self, area_comunal: AreaComunal, fecha_reserva, hora_inicio, hora_fin, tipo_reserva, ciudadano: Ciudadano):
         if self.ciudadano_supera_maximo_reservas(ciudadano):
@@ -22,3 +22,31 @@ class ServicioReserva(RespositorioReserva):
             ciudadano=ciudadano
         )
         return reserva.obtener_id(), True
+
+    def hay_areas_comunales_disponibles(self, espacio_publico):
+        return AreaComunal.objects.filter(espacio_publico=espacio_publico).count() > 0
+
+    def obtener_area_comunal(self, id_area_comunal):
+        return AreaComunal.objects.get(id=id_area_comunal)
+
+    def obtener_reserva_por_id(self, id_reserva):
+        return Reserva.objects.get(id=id_reserva)
+
+    def cancelar_reserva(self, id_reserva, ciudadano):
+        reserva = self.obtener_reserva_por_id(id_reserva)
+        if reserva.ciudadano != ciudadano:
+            return False
+        reserva.estado_reserva = 'Cancelada'
+        reserva.save()
+        return True
+    def agregar_correos_invitados_a_reserva(self, id_reserva, correos_invitados):
+        reserva = self.obtener_reserva_por_id(id_reserva)
+        reserva.correos_invitados = correos_invitados
+        reserva.save()
+        return True
+
+    def obtener_reservas_ciudadano(self, ciudadano):
+        return ciudadano.reservas.all()
+
+    def obtener_reservas_area_comunal(self, area_comunal):
+        return area_comunal.reservas.all()
