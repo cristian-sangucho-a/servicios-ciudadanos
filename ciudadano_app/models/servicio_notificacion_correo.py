1af54b2252111aa3aceb01dea8bf038be73767b3
cjzam <cristian.zambrano@epn.edu.ec>
2025-01-30 19:00:31 -0500

    feat (envio de notificaciones por correo): implementar metodos en la clase ServicioNotificacionPorCorreo y configuraciones en settings para el envio de correos con django core mail

---
+++ ciudadano_app/models/servicio_notificacion_correo.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from ciudadano_app.models.reserva.reserva import Reserva
+
+class ServicioNotificacionPorCorreo():
+
+    def enviar_invitacion(self, reserva: Reserva):
+        email = reserva.correos_invitados
+        msg = EmailMultiAlternatives(
+            'Invitación a actividad privada en espacio comunal',
+            'Hola, se ha enviado una solicitud para que asistas a una actividad en el espacio comunal en la fecha '
+            + str(reserva.fecha_reserva)
+            + ' a las '
+            + str(reserva.hora_inicio)
+            + ' hasta las '
+            + str(reserva.hora_fin)
+            + 'en el area comunal'
+            + str(reserva.area_comunal),
+            settings.EMAIL_HOST_USER,
+            [email]
+        )
+        try:
+            msg.send()
+        except:
+            # TODO: manejar el error enviandole a la vista
+            return False
+        return True
+
+    def enviar_cancelacion(self, reserva: Reserva):
+        email = reserva.correos_invitados
+        msg = EmailMultiAlternatives(
+            'Invitación a actividad privada en espacio comunal',
+            'Hola, se ha cancelado una reserva a la que hacias parte. '
+            + str(reserva.area_comunal),
+            settings.EMAIL_HOST_USER,
+            [email]
+        )
+        try:
+            msg.send()
+        except:
+            # TODO: manejar el error enviandole a la vista
+            return False
+        return True
