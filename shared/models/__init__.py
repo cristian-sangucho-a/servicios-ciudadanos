fe5b4f11346056b38bf45ab24cc261658b856ae5
Marco Márquez <m.marquez.desarrollador@outlook.com>
2025-02-01 13:23:04 -0500

    fix(notificacion_init_py): Agregar el modelo de notificacion al init

--- shared/models/__init__.py
+++ shared/models/__init__.py
@@ -7,6 +7,7 @@ from .reporte.repositorio_de_reporte import RepositorioDeReporte
 from .reporte.tipo_reporte import TipoReporte
 from .reporte.servicio_de_reporte import ServicioDeReporte
 from .reporte.repositorio_de_reporte_django import RepositorioDeReporteDjango
+from .notificacion.notificacion import Notificacion
 
-__all__ = ['Reporte', 'RepositorioDeReporte', 'TipoReporte', 'ServicioDeReporte', 'RepositorioDeReporteDjango']
+__all__ = ['Reporte', 'RepositorioDeReporte', 'TipoReporte', 'ServicioDeReporte', 'RepositorioDeReporteDjango', 'Notificacion']
 
