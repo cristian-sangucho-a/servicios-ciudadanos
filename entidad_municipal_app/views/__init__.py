8f542117824c7ca62065b8fcd61aa2814780c659
K3v1n17 <94476102+K3v1n17@users.noreply.github.com>
2025-02-01 01:01:53 -0500

    feat(views): crear vista para resolver reportes

--- entidad_municipal_app/views/__init__.py
+++ entidad_municipal_app/views/__init__.py
@@ -2,10 +2,18 @@ from .dashboard import dashboard_entidad
 from .login import login_entidad
 from .bienvenida import bienvenida_entidad
 from .eventos.lista_eventos import lista_eventos
+from .reportes.lista_reportes import lista_todos_reportes
+from .reportes.lista_reportes_por_departamento import lista_reportes_por_departamento
+from .reportes.postergar_reporte import postergar_reporte
+from .reportes.resolver_reporte import resolver_reporte
 
 __all__ = [
     'dashboard_entidad',
     'login_entidad',
     'bienvenida_entidad',
     'lista_eventos',
+    'lista_todos_reportes',
+    'lista_reportes_por_departamento',
+    'postergar_reporte',
+    'resolver_reporte'
 ]
