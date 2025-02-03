5817b7e556e48249167f39da1ae34a19c52e1147
riofutabac <alexis.lapo@epn.edu.ec>
2025-02-02 23:23:11 -0500

    feat(vista-evento): Se implemento la visa evento aparte de eso se modifico entidad municiapal la forma de obtener los ciudadnos y el actualizar estado

--- entidad_municipal_app/urls.py
+++ entidad_municipal_app/urls.py
@@ -4,6 +4,7 @@ URLs para la aplicación de entidad municipal.
 from django.urls import path
 from . import views
 from shared.views.logout import logout_usuario
+from .views.eventos.evento import actualizar_asistencia, eliminar_inscripcion
 
 urlpatterns = [
     path('', views.bienvenida_entidad, name='bienvenida_entidad'),
@@ -11,5 +12,7 @@ urlpatterns = [
     path('logout/', logout_usuario, name='logout_entidad'),
     path('dashboard/', views.dashboard_entidad, name='dashboard_entidad'),
     path('eventos/', views.gestor_eventos, name='gestor_eventos'),
-    path('eventos/<int:evento_id>/', views.evento, name='evento'),
+    path('eventos/<int:evento_id>/', views.evento, name='detalle_evento'),
+    path('eventos/asistencia/<int:registro_id>/', actualizar_asistencia, name='actualizar_asistencia'),
+    path('eventos/inscripcion/<int:registro_id>/', eliminar_inscripcion, name='eliminar_inscripcion'),
 ]
