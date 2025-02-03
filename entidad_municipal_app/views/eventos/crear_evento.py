ea13c57c054e894a90a0f0c50ccd0775e6204aba
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-02-03 01:15:35 -0500

    feat(crear_eventos): agregar la relacion con espacio publico al crear evento

--- entidad_municipal_app/views/eventos/crear_evento.py
+++ entidad_municipal_app/views/eventos/crear_evento.py
@@ -2,35 +2,34 @@ from django.shortcuts import render, redirect
 from entidad_municipal_app.models.evento.evento_municipal import EventoMunicipal
 from django.contrib.auth.decorators import login_required
 from entidad_municipal_app.decorators import entidad_required
-from django.contrib import messages  # Para mostrar mensajes
+from django.contrib import messages
+from entidad_municipal_app.models.espacio_publico import EspacioPublico
 
 @entidad_required
 @login_required
 def crear_evento(request):
+    espacios = EspacioPublico.objects.all()
     if request.method == 'POST':
-        # Obtener los datos del formulario
         nombre_evento = request.POST.get('nombre_evento')
         descripcion_evento = request.POST.get('descripcion_evento')
         fecha_realizacion = request.POST.get('fecha_realizacion')
-        lugar_evento = request.POST.get('lugar_evento')
+        espacio_publico = EspacioPublico.objects.get(pk=request.POST.get('espacio_publico'))
         capacidad_maxima = request.POST.get('capacidad_maxima')
+        entidad_municipal = request.user
 
-        # Obtener la entidad municipal del usuario autenticado
-        entidad_municipal = request.user  # Asegúrate de que esto sea correcto
-
-        # Crear el evento usando el manager
         try:
             EventoMunicipal.objects.crear_evento_con_aforo(
                 nombre=nombre_evento,
                 descripcion=descripcion_evento,
                 fecha=fecha_realizacion,
-                lugar=lugar_evento,
+                lugar=espacio_publico.direccion,
                 capacidad=capacidad_maxima,
-                entidad_municipal=entidad_municipal
+                entidad_municipal=entidad_municipal,
+                espacio_publico=espacio_publico
             )
             messages.success(request, 'El evento se ha creado exitosamente.')
-            return redirect('gestor_eventos')  # Redirigir a la lista de eventos
+            return redirect('gestor_eventos')
         except Exception as e:
             messages.error(request, f'Error al crear el evento: {str(e)}')
 
-    return render(request, 'entidad/eventos/crear_evento.html')
+    return render(request, 'entidad/eventos/crear_evento.html', {'espacios': espacios})
