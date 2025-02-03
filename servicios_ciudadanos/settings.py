e1a4d121f4f5f7a11b285ed5622f857458a43bd1
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-02-03 07:33:43 -0500

    chore(static): agregar dependencia de htmx para consultas dinamicas

--- servicios_ciudadanos/settings.py
+++ servicios_ciudadanos/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'shared',
     'tailwind',
     'theme',
+    'django_htmx',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -71,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'servicios_ciudadanos.urls'
