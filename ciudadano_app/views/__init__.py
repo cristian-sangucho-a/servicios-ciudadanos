5728c6b04a3c6e7bc70d65ff0d212c7e85379011
Kevin Sebastian D <kedose@outlook.it>
2025-02-03 15:52:01 -0500

    feat(notificaciones): Crear botones para templates creados en bienvenida.html
    Co-Authored-By: Cantx08 <109187665+Cantx08@users.noreply.github.com>
    Co-Authored-By: Jonathan Poaquiza <109117858+jonathanpoaquiza75@users.noreply.github.com>

--- ciudadano_app/views/__init__.py
+++ ciudadano_app/views/__init__.py
@@ -1,4 +1,8 @@
 from .dashboard import dashboard_ciudadano
+from .notificaciones import notificacion_de_estado
+from .notificaciones.agregar_sectores import agregar_sectores_ciudadano
+from .notificaciones.notificacion_de_estado import notificar_estado_riesgo_ciudadano, listar_notificaciones_ciudadano, \
+    notificar_reporte_alta_prioridad_ciudadano
 from .registro import registro_ciudadano
 from .login import login_ciudadano
 from .bienvenida import bienvenida_ciudadano
@@ -12,4 +16,9 @@ __all__ = [
     'bienvenida_ciudadano',
     'lista_eventos',
     'envio_reporte',
+    'agregar_sectores_ciudadano',
+    'listar_notificaciones_ciudadano',
+    'notificar_reporte_alta_prioridad_ciudadano',
+    'notificar_estado_riesgo_ciudadano',
+
 ]
