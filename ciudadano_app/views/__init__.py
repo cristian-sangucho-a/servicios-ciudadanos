3dfe899e711ecd0b62c3535ee4e083c017805625
Juan F. Carrillo <78522923+juanfcarrillo@users.noreply.github.com>
2025-02-02 20:56:51 -0500

    feat(reporte): added view for reporte (#37)

--- ciudadano_app/views/__init__.py
+++ ciudadano_app/views/__init__.py
@@ -2,10 +2,12 @@ from .dashboard import dashboard_ciudadano
 from .registro import registro_ciudadano
 from .login import login_ciudadano
 from .bienvenida import bienvenida_ciudadano
+from .reporte.envio_reporte import envio_reporte
 
 __all__ = [
     'dashboard_ciudadano',
     'registro_ciudadano',
     'login_ciudadano',
     'bienvenida_ciudadano',
+    'envio_reporte',
 ]
