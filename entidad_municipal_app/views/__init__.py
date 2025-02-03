8edef2f3d9e171c0c6f3c730cbbe0a1257367ece
Jonathan Salazar <120618497+jluSalazar@users.noreply.github.com>
2025-02-02 23:04:41 -0500

    feat(views): Views & Templates manejar_reportes (#35)
    Co-authored-by: K3v1n17 <94476102+K3v1n17@users.noreply.github.com>

--- entidad_municipal_app/views/__init__.py
+++ entidad_municipal_app/views/__init__.py
@@ -2,10 +2,20 @@ from .dashboard import dashboard_entidad
 from .login import login_entidad
 from .bienvenida import bienvenida_entidad
 from .eventos.lista_eventos import lista_eventos
+from .reportes.lista_reportes import lista_todos_reportes
+from .reportes.lista_reportes_por_departamento import lista_reportes_por_departamento
+from .reportes.postergar_reporte import postergar_reporte
+from .reportes.resolver_reporte import resolver_reporte
+from .reportes.agregar_evidencia import agregar_evidencia
 
 __all__ = [
     'dashboard_entidad',
     'login_entidad',
     'bienvenida_entidad',
     'lista_eventos',
+    'lista_todos_reportes',
+    'lista_reportes_por_departamento',
+    'postergar_reporte',
+    'resolver_reporte',
+    'agregar_evidencia'
 ]
