4227533d9b6bd84529382998048e86d3b101c0b2
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-02-02 23:01:02 -0500

    refactor(models): cambiar la firma del metodo para recibir los correos de os invitados

--- ciudadano_app/models/reserva/servicio_reserva.py
+++ ciudadano_app/models/reserva/servicio_reserva.py
@@ -1,5 +1,3 @@
-from pyexpat.errors import messages
-
 from ciudadano_app.models import Ciudadano, AreaComunal, Reserva
 from ciudadano_app.models.reserva.repositorio_reserva import RespositorioReserva
 from ciudadano_app.models.servicio_notificacion_correo import ServicioNotificacionPorCorreo
@@ -10,23 +8,28 @@ class ServicioReserva(RespositorioReserva):
     MAXIMO_RESERVAS = 3
 
     def ciudadano_supera_maximo_reservas(self, ciudadano: Ciudadano):
-        print(ciudadano.reservas.filter(estado_reserva='Activa').count())
         return ciudadano.reservas.filter(estado_reserva='Activa').count() > self.MAXIMO_RESERVAS
 
     def reservar_area_comunal(self, area_comunal: AreaComunal, fecha_reserva, hora_inicio, hora_fin, tipo_reserva,
-                              ciudadano: Ciudadano):
-        print(self.ciudadano_supera_maximo_reservas(ciudadano))
+                              ciudadano: Ciudadano, correos_invitados):
         if self.ciudadano_supera_maximo_reservas(ciudadano):
             return 0, False
-        #TODO: verificar que el espacio publico al que pertenece el area comunal este libre para reservar
-        reserva = Reserva.objects.create(
+        reserva = Reserva(
             area_comunal=area_comunal,
             fecha_reserva=fecha_reserva,
             hora_inicio=hora_inicio,
             hora_fin=hora_fin,
+            estado_reserva='Activa',
             tipo_reserva=tipo_reserva,
-            ciudadano=ciudadano
+            ciudadano=ciudadano,
+            correos_invitados=correos_invitados
         )
+        reserva.save()
+        if tipo_reserva == 'privado':
+            print('Enviando correos')
+            servicio_notificion_correo = ServicioNotificacionPorCorreo()
+            servicio_notificion_correo.enviar_invitacion(reserva)
+            print('Correos enviados')
         return reserva.obtener_id(), True
 
     def hay_areas_comunales_disponibles(self, espacio_publico):
@@ -61,41 +64,10 @@ class ServicioReserva(RespositorioReserva):
         return ciudadano.reservas.filter(estado_reserva='Activa')
 
     def obtener_reservas_area_comunal(self, area_comunal, fecha):
-        return area_comunal.reservas.filter(fecha_reserva=fecha, estado_reserva='Activa')
+        return area_comunal.reservas.filter(fecha_reserva=fecha)
 
-    def reservar(self, area_comunal: AreaComunal, fecha_reserva, hora_inicio,
-                                                     hora_fin, tipo_reserva,
-                                                     ciudadano: Ciudadano, correos_invitados):
-        print(self.ciudadano_supera_maximo_reservas(ciudadano))
 
-        if self.ciudadano_supera_maximo_reservas(ciudadano):
-            print(self.ciudadano_supera_maximo_reservas(ciudadano))
-            return 0, False
 
-        #TODO: verificar que el espacio publico al que pertenece el area comunal este libre para reservar
-        reserva = Reserva(
-            area_comunal=area_comunal,
-            fecha_reserva=fecha_reserva,
-            hora_inicio=hora_inicio,
-            hora_fin=hora_fin,
-            estado_reserva='Activa',
-            tipo_reserva=tipo_reserva,
-            ciudadano=ciudadano,
-            correos_invitados=correos_invitados
-        )
-
-        if not self.existe_reserva(reserva):
-            reserva.save()
-        else:
-            return 0, False
-
-        if tipo_reserva == 'privado':
-            servicio_notificion_correo = ServicioNotificacionPorCorreo()
-            servicio_notificion_correo.enviar_invitacion(reserva)
-        return reserva.obtener_id(), True
-
-    def existe_reserva(self, reserva):
-        return Reserva.objects.filter(area_comunal=reserva.area_comunal,fecha_reserva=reserva.fecha_reserva, hora_inicio=reserva.hora_inicio, hora_fin=reserva.hora_fin, estado_reserva='Activa').exists()
 
     def cancelar_reserva_privada(self, id_reserva, ciudadano):
         reserva = self.obtener_reserva_por_id(id_reserva)
@@ -106,6 +78,8 @@ class ServicioReserva(RespositorioReserva):
         servicio_notificion_correo = ServicioNotificacionPorCorreo()
         servicio_notificion_correo.enviar_cancelacion(reserva)
         return True
+
+    ##PARA RECUPERAR DATOS DESDE LA VISTA
 
     def obtener_espacios_publicos(self):
         return EspacioPublico.objects.all()
--- ciudadano_app/views/reserva.py
+++ ciudadano_app/views/reserva.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from ciudadano_app.decorators import ciudadano_required
 from ciudadano_app.forms import ReservaRegisterForm
+from ciudadano_app.models import AreaComunal
 from ciudadano_app.models.ciudadano.ciudadano import Ciudadano
 from ciudadano_app.models.reserva.servicio_reserva import ServicioReserva
 
@@ -9,26 +12,49 @@ from ciudadano_app.models.reserva.servicio_reserva import ServicioReserva
 @ciudadano_required
 def reservar(request):
     ciudadano: Ciudadano = request.user
-    servicio_reserva = ServicioReserva()
 
-    fecha_reserva = request.GET.get('fecha_reserva', '')
-    hora_inicio = request.GET.get('hora_inicio', '')
-    hora_fin = request.GET.get('hora_fin', '')
-    area_comunal_id = request.GET.get('area_comunal', '')
-    area_comunal = servicio_reserva.obtener_area_comunal(area_comunal_id)
+    try:
+        fecha_reserva = request.GET.get('fecha_reserva', '')
+        hora_inicio = request.GET.get('hora_inicio', '')
+        hora_fin = request.GET.get('hora_fin', '')
+        area_comunal_id = request.GET.get('area_comunal', '')
+
+        if not all([fecha_reserva, hora_inicio, hora_fin, area_comunal_id]):
+            raise ValueError("Parámetros incompletos en la URL")
+
+    except ValueError as e:
+        messages.error(request, str(e))
+        return redirect('pagina_error')
+
+    try:
+        area_comunal = AreaComunal.objects.get(pk=area_comunal_id)
+    except (AreaComunal.DoesNotExist, ValueError):
+        messages.error(request, "Área comunal no válida")
+        return redirect('cargar_calendario')
 
     if request.method == 'POST':
         form = ReservaRegisterForm(request.POST)
+        print(form.is_valid())  # Debe mostrar False/True
+        print(form.errors)  # Mostrar errores en consola
+
         if form.is_valid():
-            id_reserva, exito = servicio_reserva.reservar(area_comunal, fecha_reserva, hora_inicio, hora_fin, form.cleaned_data['tipo_reserva'], ciudadano, form.cleaned_data['correos_invitados'])
-            if exito:
-                messages.success(request, '¡Reserva creada exitosamente!')
-                return redirect('mis_reservas')
-            else:
-                messages.error(request, 'No se pudo crear la reserva')
-                return redirect('agenda')
+            try:
+                servicio_reserva = ServicioReserva()
+                id_reserva, exito = servicio_reserva.reservar_area_comunal(area_comunal, fecha_reserva, hora_inicio, hora_fin, form.cleaned_data['tipo_reserva'], ciudadano, form.cleaned_data['correos_invitados'])
+                print(exito)
+                if exito:
+                    messages.success(request, '¡Reserva creada exitosamente!')
+                    return redirect('mis_reservas')
+                return redirect('cargar_calendario')
+
+            except Exception as e:
+                messages.error(request, f"Error crítico: {str(e)}")
+                return redirect('cargar_calendario')
         else:
-            messages.error(request, 'No se lleno correctamente el formulario.')
+            # Agregar todos los errores a messages
+            for campo, errores in form.errors.items():
+                for error in errores:
+                    messages.error(request, f"Error en {campo}: {error}")
 
     else:
         initial_data = {
@@ -53,7 +79,9 @@ def cancelar_reserva(request):
 
     if request.method == 'POST':
         id_reserva =  request.POST.get('id_reserva')
+
         servicio_reserva = ServicioReserva()
+
         servicio_reserva.cancelar_reserva(id_reserva, ciudadano)
 
     return redirect('mis_reservas')
