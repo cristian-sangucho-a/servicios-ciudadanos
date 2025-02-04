ae5bba480f5a4a303376a5bc607b1cc7a521b09b
Jonathan Salazar <120618497+jluSalazar@users.noreply.github.com>
2025-02-03 20:45:04 -0500

    feat(reporte_municipal): init data para reportes municipales (#41)

--- entidad_municipal_app/views/reportes/postergar_reporte.py
+++ entidad_municipal_app/views/reportes/postergar_reporte.py
@@ -2,14 +2,15 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 from entidad_municipal_app.decorators import entidad_required
-from mocks.repositorio_de_reporte_municipal_en_memoria import RepositorioDeReporteMunicipalEnMemoria
+from entidad_municipal_app.models.reporte.repositorio_de_reporte_municipal_django import \
+    RepositorioDeReporteMunicipalDjango
 from entidad_municipal_app.models.reporte.servicio_de_reporte_municipal import ServicioReporteMunicipal
 
 @entidad_required
 def postergar_reporte(request, reporte_id):
     if request.method == "POST":
         # Inicializar el repositorio y el servicio de reporte municipal
-        repositorio = RepositorioDeReporteMunicipalEnMemoria()
+        repositorio = RepositorioDeReporteMunicipalDjango()
         servicio_reporte = ServicioReporteMunicipal(repositorio)
 
         # Obtener el reporte a través del servicio
@@ -17,6 +18,7 @@ def postergar_reporte(request, reporte_id):
 
         try:
             servicio_reporte.postergar_reporte(reporte_id)
+            messages.success(request, f'El reporte #{reporte_id} se ha sido postergado.')
         except Exception as e:
             print(f"Error {e}")
             estado = reporte.obtener_estado()
