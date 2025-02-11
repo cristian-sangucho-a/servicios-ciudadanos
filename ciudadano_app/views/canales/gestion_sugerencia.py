37970414bc2d233ce07e403dbc8dde0b6a4cdbc0
WooshC <140362091+WooshC@users.noreply.github.com>
2025-02-10 19:54:12 -0500

    feat(refactor/canales_municipales):Validaciones

--- ciudadano_app/views/canales/gestion_sugerencia.py
+++ ciudadano_app/views/canales/gestion_sugerencia.py
@@ -1,33 +1,40 @@
 from contextlib import nullcontext
-
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 
 from entidad_municipal_app.models import EntidadMunicipal
 from entidad_municipal_app.models.canales.sugerencia import Sugerencia
 
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
 
 def crear_sugerencia(request):
     """
-        Crea un nuevo canal informativo y lo suscribe a todos los ciudadanos si es de emergencia.
-
-        Esta vista maneja el formulario de creación de un canal. Al recibir una solicitud POST, crea un nuevo canal
-        informativo basado en los datos enviados en el formulario.
-        Args:
-            request (HttpRequest): La solicitud HTTP realizada por el usuario que contiene los datos del formulario.
-
-        Returns:
-            HttpResponseRedirect: Redirige a la lista de canales de la entidad municipal después de crear el canal.
+    Crea un nuevo canal informativo y lo suscribe a todos los ciudadanos si es de emergencia.
     """
     if request.method == 'POST':
         ciudadano = request.user
         entidad_municipal = EntidadMunicipal.objects.get(id=1)
         nombre = request.POST.get('nombre_sugerencia')
         descripcion = request.POST.get('descripcion')
-        if nombre and descripcion:
-            Sugerencia.crear_sugerencia_canal(nombre,descripcion, ciudadano, entidad_municipal)
 
-    print(Sugerencia.objects.all())
-    return redirect('/ciudadano/dashboard')
+        # Verificar si ya existe un canal con el mismo nombre
+        if Sugerencia.objects.filter(nombre=nombre).exists():
+            # Pasar el mensaje de error en el contexto
+            return render(request, 'canales/crear_sugerencia.html', {
+                'nombre_sugerencia': nombre,
+                'descripcion': descripcion,
+                'modal_error': 'Ya existe un canal con este nombre. Por favor, elige otro nombre.',
+            })
+        elif nombre and descripcion:
+            Sugerencia.crear_sugerencia_canal(nombre, descripcion, ciudadano, entidad_municipal)
+            messages.success(request, 'La sugerencia de canal ha sido creada exitosamente.')
+            return redirect('/ciudadano/dashboard')
+
+    # Si no es POST, mostrar el formulario vacío
+    return render(request, 'canales/crear_sugerencia.html')
 
 def crear_sugerencia_form(request):
     """
